08a4cd0b41e245649ab1ad1182a2c39a35acb1fe
= <=>
2024-05-22 23:35:58 +0200

    Correction bug non Ajout
    correction du bug qui empêchait l'ajout de nouvelle ames
    
    pas de update_user_meta dans __associate...

--- wp-content/plugins/poimen/src/WPFormObject.php
+++ wp-content/plugins/poimen/src/WPFormObject.php
@@ -87,6 +87,7 @@ class WPFormObject {
             'reminder_level' => 0 // Ajout de cet attribut pour gerer les rappels. Cela sert à inviter de spamer
                                                     // les L.A avec des rappels des mêmes âmes à chaque email
         );
+        update_user_meta($leaderID, 'associated_clients', $leaderSouls);
     }
     
     public function __removeSoulFromLeader(int $leaderID, string $soulName) {
